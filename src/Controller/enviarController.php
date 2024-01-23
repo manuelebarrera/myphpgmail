f95ff6263d77246bfea8ddbf74cbf8a86b048822
manuelebarrera <manuelebarrera@gmail.com>
2024-01-23 18:17:03 -0300

    creacion de archivo para guardar credenciales

--- src/Controller/enviarController.php
+++ src/Controller/enviarController.php
@@ -10,6 +10,24 @@ use App\Service\GmailService;
 use Google\Service\Gmail; //
 use Google_Client;
 
+
+//use Google\Auth\Credentials\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Importa las clases necesarias
 
 use Google_Service;  //x
@@ -224,112 +242,168 @@ if (isset($_GET['code']) && $_SERVER['REQUEST_URI'] == $redirect_uri) {
 
 
 
-/**
- * @Route("/email4", name="send_email4")
- */
-public function sendEmail4()
-{
-    // Configura las credenciales
-    $client_id = "653487830785-9tei5cco9g1oivhjs4c26tv2kjlv8m51.apps.googleusercontent.com";
-    $client_secret = "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";
-    $redirect_uri = 'https://127.0.0.1:8001/email4';
-    
-    // Crea el cliente de Google
-    $client = new Google_Client();
-    $client->setClientId($client_id);
-    $client->setClientSecret($client_secret);
-    $client->setRedirectUri($redirect_uri);
-    $client->addScope([Gmail::MAIL_GOOGLE_COM]);
+    /**
+     * @Route("/email4", name="send_email4")
+     */
+    public function sendEmail4()
+    {
+        $credentialspath = (__DIR__ . '/token.json');
+        // Configura las credenciales
+        $client_id = "653487830785-9tei5cco9g1oivhjs4c26tv2kjlv8m51.apps.googleusercontent.com";
+        $client_secret = "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";
+        $redirect_uri = 'https://127.0.0.1:8001/email4';
 
-    // Genera la URL de autenticación
-    $auth_url = $client->createAuthUrl();
+        // Crea el cliente de Google
+        $client = new Google_Client();
+        $client->setClientId($client_id);
+        $client->setClientSecret($client_secret);
+        $client->setRedirectUri($redirect_uri);
+        $client->addScope([Gmail::MAIL_GOOGLE_COM]);
 
-    // Redirige al usuario a la URL de autenticación
-    header('Location: ' . $auth_url);
-    
-    // Captura la URL de devolución
-    if (isset($_GET['code'])) {
-        $code = $_GET['code'];
-
-        // Intercambia el código de autorización por un token de acceso
-        $access_token = $client->fetchAccessTokenWithAuthCode($code);
-
-        // Usa el token de acceso para realizar una solicitud a la API
-        $service = new Gmail($client);
+        $client->setAccessType('offline');
 
 
-         // Enviar correo electrónico de prueba
-    $to = 'destinatario@example.com';
-    $subject = 'Asunto del correo electrónico';
-    $body = 'Este es el cuerpo del correo electrónico';
 
-    //$service->users->messages->send($to, $subject, $body);
-    
-   // $service->users_messages->send($to, $subject, $body);
 
-    $result = $service->users_messages->listUsersMessages('me', ['maxResults'=> 100]);
-    
-    
-    print("##########   CONECTADO API GMAIL   ##########" . "<br><br>");
-    
-   
+        if (file_exists($credentialspath)) {
+            $access_token = json_decode(file_get_contents($credentialspath), true);
+        } else {
 
-    foreach ($result->getMessages() as $msg) {
-        $messageId = $msg->getId();
-        $message = $service->users_messages->get('me', $messageId);
-        $headers = $message->getPayload()->getHeaders();
-        $subject = "";
-        $date = "";
-        $snippet = $message->getSnippet();
-    
-        foreach ($headers as $header) {
-            if ($header->getName() == 'Subject') {
-                $subject = $header->getValue();
-            }
-            if ($header->getName() == 'Date') {
-                $date = $header->getValue();
+
+
+            // Genera la URL de autenticación
+            $auth_url = $client->createAuthUrl();
+
+            // Redirige al usuario a la URL de autenticación
+            header('Location: ' . $auth_url);
+
+            // Captura la URL de devolución
+            if (isset($_GET['code'])) {
+                $code = $_GET['code'];
+
+                // Intercambia el código de autorización por un token de acceso
+                $access_token = $client->fetchAccessTokenWithAuthCode($code);
+                //$xxx=$client->getAccessToken();
+
+                //guardar en disco el token
+                //$credentialspath = (__DIR__.'/token.json');
+
+
+                if (!file_exists(dirname($credentialspath))) {
+                    mkdir(dirname($credentialspath), 0700, true);
+                }
+                file_put_contents($credentialspath, json_encode($access_token));
+
+
+
+                $client->setAccessToken($access_token);
+                if ($client->isAccessTokenExpired()) {
+                    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                    file_put_contents($credentialspath, json_encode($client->getAccessToken()));
+                }
+
+                // Usa el token de acceso para realizar una solicitud a la API
+                $service = new Gmail($client);
+
+
+                // Enviar correo electrónico de prueba
+                $to = 'destinatario@example.com';
+                $subject = 'Asunto del correo electrónico';
+                $body = 'Este es el cuerpo del correo electrónico';
+
+                //$service->users->messages->send($to, $subject, $body);
+
+                // $service->users_messages->send($to, $subject, $body);
+
+                $result = $service->users_messages->listUsersMessages('me', ['maxResults' => 100]);
+
+
+                print("##########   CONECTADO API GMAIL   ##########" . "<br><br>");
+
+
+
+                foreach ($result->getMessages() as $msg) {
+                    $messageId = $msg->getId();
+                    $message = $service->users_messages->get('me', $messageId);
+                    $headers = $message->getPayload()->getHeaders();
+                    $subject = "";
+                    $date = "";
+                    $snippet = $message->getSnippet();
+
+                    foreach ($headers as $header) {
+                        if ($header->getName() == 'Subject') {
+                            $subject = $header->getValue();
+                        }
+                        if ($header->getName() == 'Date') {
+                            $date = $header->getValue();
+                        }
+                    }
+
+                    print("ID del mensaje: " . $messageId . "<br>");
+                    print("Asunto: " . $subject . "<br>");
+                    print("Fecha: " . $date . "<br>");
+                    print("Extracto: " . $snippet . "<br><br>");
+                }
+
+
+                // usuario se ha conectado con exito la primera vez
+
+
+
+
+
+
+                // Mensaje de éxito
+                $response = new Response('Conectado a la api de gmail  ');
+                return $response;
+            } else {
+                // Mensaje de error
+
+
+                $auth_url = $client->createAuthUrl();
+
+                // Redirige al usuario a la URL de autenticación
+                header('Location: ' . $auth_url);
+                $response = new Response('ERROR en api de gmail  ');
+                exit();
+                return $response;
             }
         }
-    
-        print("ID del mensaje: " . $messageId . "<br>");
-        print("Asunto: " . $subject . "<br>");
-        print("Fecha: " . $date . "<br>");
-        print("Extracto: " . $snippet . "<br><br>");
+        $client->setAccessToken($access_token);
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            file_put_contents($credentialspath, json_encode($client->getAccessToken()));
+        }
+        $service = new Gmail($client);
+        $result = $service->users_messages->listUsersMessages('me', ['maxResults' => 100]);
+        foreach ($result->getMessages() as $msg) {
+            $messageId = $msg->getId();
+            $message = $service->users_messages->get('me', $messageId);
+            $headers = $message->getPayload()->getHeaders();
+            $subject = "";
+            $date = "";
+            $snippet = $message->getSnippet();
+
+            foreach ($headers as $header) {
+                if ($header->getName() == 'Subject') {
+                    $subject = $header->getValue();
+                }
+                if ($header->getName() == 'Date') {
+                    $date = $header->getValue();
+                }
+            }
+
+            print("ID del mensaje: " . $messageId . "<br>");
+            print("Asunto: " . $subject . "<br>");
+            print("Fecha: " . $date . "<br>");
+            print("Extracto: " . $snippet . "<br><br>");
+        }
+
+
+
+
+
+        $response = new Response('Conectado a la api de gmail final ');
+            return $response;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-        // Mensaje de éxito
-        $response = new Response('Conectado a la api de gmail  ');
-        return $response;
-    } else {
-        // Mensaje de error
-       
-       
-    $auth_url = $client->createAuthUrl();
-  
-    // Redirige al usuario a la URL de autenticación
-    header('Location: ' . $auth_url);
-    $response = new Response('ERROR en api de gmail  ');
-    exit();
-    return $response;
-
-
-    }
-}
-
-
-
-
 }
